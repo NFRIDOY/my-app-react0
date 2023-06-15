50f3dc562a2533aa0e5f221bea19e32d2882226d
NF RIDOY <nfridoy@gmail.com>
2023-06-15 22:39:55 +0600

    Nothing chaged

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 // import logo from './logo.svg';
 import './App.css';
 import MyNav from './components/MyNav';
-import second from './components/MyNav'
 
 import Navbar from './components/Navbar';
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,48 @@
-// navber
-import React from 'react'
+// navber rfc
+import React from 'react';
+
+
 
 export default function Navbar(props) {
     return (
         <>
             {/* <div>Navbar</div> */}
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <a className="navbar-brand" href="#">Navbar</a>
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav mr-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Link</a>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a className="dropdown-item" href="#">Action</a>
+                                <a className="dropdown-item" href="#">Another action</a>
+                                <div className="dropdown-divider"></div>
+                                <a className="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                    <form className="form-inline my-2 my-lg-0">
+                        <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+                        <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </div>
+            </nav>
+            {/*  */}
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <a className="navbar-brand" href="/">{props.title}</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
@@ -21,12 +59,26 @@ export default function Navbar(props) {
                         <li className="nav-item">
                             <a className="nav-link" href="/">Pricing</a>
                         </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="/">{props.Login}</a>
+                        </li>
                     </ul>
-                    <span className="navbar-text">
-                        Navbar text with an inline element
+                    <span className="navbar-text container d-flex flex-row-reverse">
+                        <span>Navbar text with an inline element</span>
                     </span>
                 </div>
             </nav>
         </>
     )
 }
+
+Navbar.defaultProps = {
+    title: "Set Title",
+    Login: "Signup/Login"
+}
+
+// Navbar.propTypes = {
+//     title : PropTypes.string,
+//     about : PropTypes.string
+// }
+
